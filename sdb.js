315ae5cb0c8be3c967c4eda738b0ee1d8b5b2dc7
andiandiandi <kaffeeflow40@web.de>
2016-10-05 22:29:29 +0200

    modified sdb.js

--- sdb.js
+++ sdb.js
@@ -1,5 +1,5 @@
 const exec = require('child_process').exec;
-const child = exec('mongod --dbpath="C:/Users/Andi/nodejsapps/nodebeginner/db"',
+const child = exec('mongod --dbpath="C:/Users/Andi//db"',
     (error, stdout, stderr) => {
         
 });
